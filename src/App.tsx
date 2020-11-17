f9ad6dfc3e9cc6f09e36b0316269ffad2056905e
tahiru94 <tahiru1994@engineer.com>
2020-11-17 13:42:56 -0500

    Add the styled-components dependency
    * Integrate styled-components into App and Search (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 
 // Container imports
 import { Autocomplete } from './containers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Wrapper className="App">
+      <Header className="App-header">
         <h3>Autocomplete API</h3>
         <p>
           Enter a search term and click "Search" to query the API
         </p>
-      </header>
+      </Header>
       <Autocomplete />
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled(`div`)`
+
+`;
+
+const Header = styled(`header`)`
+
+`;
 
 export default App;
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Input, Button } from '@material-ui/core';
+import styled from 'styled-components';
 
 class Search extends Component {
 
@@ -10,12 +11,29 @@ class Search extends Component {
 
     render() {
         return (
-            <div>
-                <Input onChange={(e) => this.onInput(e)} placeholder="Enter search term" />
-                <Button variant="contained" color="primary">Search</Button>
-            </div>
+            <Wrapper>
+                <Item>
+                    <Input onChange={(e) => this.onInput(e)} placeholder="Enter search term" />
+                </Item>
+                <Item>
+                    <Button variant="contained" color="primary">Search</Button>
+                </Item>
+            </Wrapper>
         );
     }
 }
+
+const Wrapper = styled(`div`)`
+    width: 100vw;
+    height: 240px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Item = styled(`div`)`
+    margin: 0 12px;
+`;
 
 export default Search;
